594869364096695fb4d9fd37f023e03b2491d15d
Subhajit Mahapatra <subha05cs@gmail.com>
2026-06-16 23:04:54 +0530

    Update registration.py

--- registration.py
+++ registration.py
@@ -1,19 +1,23 @@
-# CA/registration.py
-
 import os
-
+import json
 from Crypto.PublicKey import ECC
-from vehicle_db import vehicles
 
 KEY_DIR = "Keys"
+VEHICLE_FILE = "vehicles.json"
 
 os.makedirs(KEY_DIR, exist_ok=True)
+
+if not os.path.exists(VEHICLE_FILE):
+    with open(VEHICLE_FILE, "w") as f:
+        json.dump({}, f)
 
 
 def register_vehicle(vehicle_id):
 
-    if vehicle_id in vehicles:
+    with open(VEHICLE_FILE, "r") as f:
+        vehicles = json.load(f)
 
+    if vehicle_id in vehicles:
         print("Vehicle already registered")
         return
 
@@ -30,13 +34,9 @@ def register_vehicle(vehicle_id):
     )
 
     with open(private_file, "wt") as f:
-
-        f.write(
-            key.export_key(format="PEM")
-        )
+        f.write(key.export_key(format="PEM"))
 
     with open(public_file, "wt") as f:
-
         f.write(
             key.public_key().export_key(
                 format="PEM"
@@ -47,6 +47,9 @@ def register_vehicle(vehicle_id):
         "private_file": private_file,
         "public_file": public_file
     }
+
+    with open(VEHICLE_FILE, "w") as f:
+        json.dump(vehicles, f, indent=4)
 
     print(
         f"{vehicle_id} registered successfully"
